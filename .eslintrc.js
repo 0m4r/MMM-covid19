e76eaa4f1cc3901541a900fa6e11be9eeb6bb893
Omar Adobati <omar.adobati@gmail.com>
2021-04-02 16:46:49 +0200

    Add linting, prettier and editorconfig + refactoring
    Refactor node_helper.js to handle the requests to to the APIs
    letting MMM-covid19.js only take care of the presentation layer.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+  },
+  plugins: [
+    'prettier'
+  ]
+};
