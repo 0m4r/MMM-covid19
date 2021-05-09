7cc91fa6ac434d5ef165e89aa0b5b9488cc553cd
Omar Adobati <omar.adobati@gmail.com>
2021-05-09 12:26:34 +0200

    feat: add node-schedule

--- README.md
+++ README.md
@@ -2,22 +2,47 @@
 Magic Mirror Module to display stats on Covid19.
 Data is provided by _https://covid19api.com/_
 
-The data shown in the table displays the total numbers (top) and the difference between today's numbers and the day before numbers (bottom).
+The data shown in the table displays the total numbers (top) and the difference calculated on the last 24 hours of available data. Reference timezone is UTC.
 
 ## configuration
+the default configuration looks like this:
 ```js
 {
   disabled: false,
   module: "MMM-covid19",
   position: "bottom_center",
-  config: {
-    updateInterval: 12 * 60 * 60 *1000, // 12 hours
-    countryCodes: ["DE", "IT", "HK"],
+  defaults: {
+    countryCodes: ['DE', 'IT'],
+    world: false,
     live: true,
-    world: true
-  }
+    updateInterval: 24 * 60 * 60 * 1000,
+    useScheduler: false,
+    schedulerConfig: '0 0 */12 * * */1'
+  },
 }
 ```
 
+- `countryCodes`: an Array of ISO2 values.
+A list cen be obtain at this url: https://api.covid19api.com/countries
+
+- `world`: a boolean value (`true` or `false`).
+Set to `true`it shows the world total numbers, it does not otherwise.
+
+- `live`: a boolean value (`true` or `false`).
+Set to `true`it shows the total numbers per country set in `countryCodes`, it does not otherwise.
+
+- `updateInterval`: a number in milliseconds to be used to refresh the numbers. Its value is only used when `useScheduler=false`.
+Its default values invokes an API call every 24 hours from starting from the first one.
+
+- `useScheduler`: a boolean value (`true` or `false`).
+Set to `true` it uses _cron_ like approach to refresh the data. It uses [`node-schedule`](https://github.com/node-schedule/node-schedule), in order to be able to use thi option, you need to run `npm install`
+
+- `schedulerConfig`: a valid `node-schedule`configuration as a string or as an object.
+Its default value execute an API call _twice a day every day ad 12am and 12pm UTC time_.
+
+
+The numbers shown are the delta between last day 00:00 UTC and the day before 00:00 UTC. The dates can be shown at the bottom of the list.
+
 ## screenshots
 ![MMM-covid19](docs/MMM-covid19.png)
+
--- node_helper.js
+++ node_helper.js
@@ -13,12 +13,16 @@ const Log = require('../../js/logger.js');
 module.exports = NodeHelper.create({
   interval: null,
   defaults: {
-    updateInterval: 1 * 60 * 60 * 1000, // 1 hours
-    countryCodes: [],
-    world: true,
+    countryCodes: ['DE', 'IT'],
+    world: false,
     live: true,
+    updateInterval: 24 * 60 * 60 * 1000, // 24 hours (only used if useScheduler=false)
+    useScheduler: false,
+    schedulerConfig: '0 0 */12 * * */1' //00 and 12 of every dayOfWeek (only used if useScheduler=true)
   },
   baseURL: 'https://api.covid19api.com',
+  scheduler: null,
+  config: {},
 
   start: function () {
     Log.log('Starting node helper for: ' + this.name);
@@ -26,6 +30,9 @@ module.exports = NodeHelper.create({
 
   stop: function () {
     Log.log('Stopping node helper for: ' + this.name);
+    if (this.scheduler !== null) {
+      this.scheduler.cancel()
+    }
   },
 
   getFromTo: function (delta = 2) {
@@ -51,8 +58,8 @@ module.exports = NodeHelper.create({
     const qs = this.getFromTo();
     countryCodes.forEach((countryCode) => {
       const url = `${this.baseURL}/total/country/${countryCode}`;
-      Log.debug(this.name, 'fetchTotal', url, JSON.stringify(qs));
-      const req = new Promise((resolve, reject) =>
+      Log.info(this.name, 'fetchTotal', url, JSON.stringify(qs));
+      const req = new Promise((resolve, _reject) =>
         request(
           {
             rejectUnauthorized: false,
@@ -63,9 +70,10 @@ module.exports = NodeHelper.create({
           (error, response, body) => {
             if (!error && response.statusCode === 200) {
               resolve({ countryCode, body: JSON.parse(body) });
+            } else {
+              Log.error(this.name, 'fetchTotal', countryCode, error);
+              resolve({ countryCode, body: [] });
             }
-            Log.error(this.name, error);
-            reject({ countryCode, body: [] });
           }
         )
       );
@@ -75,9 +83,9 @@ module.exports = NodeHelper.create({
 
     Promise.all(requests)
       .then((res) => {
-        this.sendSocketNotification('TOTAL_RESULTS', res);
+        this.sendSocketNotification(this.name + 'TOTAL_RESULTS', res);
       })
-      .catch(() => this.sendSocketNotification('TOTAL_RESULTS', []));
+      .catch(() => this.sendSocketNotification(this.name + 'TOTAL_RESULTS', []));
   },
 
   fetchWorld: function () {
@@ -100,8 +108,10 @@ module.exports = NodeHelper.create({
           response.statusCode === 200
         ) {
           results = JSON.parse(body)[0];
+        } else {
+          Log.error(this.name, 'fetchWorld', error);
         }
-        this.sendSocketNotification('WORLD_RESULTS', {
+        this.sendSocketNotification(this.name + 'WORLD_RESULTS', {
           countryCode: 'world',
           body: results,
         });
@@ -124,8 +134,10 @@ module.exports = NodeHelper.create({
         if (!error && response.statusCode === 200) {
           const results = JSON.parse(body);
           remote = results.version || 0;
+        } else {
+          Log.error(this.name, 'fetchVersion', error);
         }
-        this.sendSocketNotification('VERSION_RESULTS', {
+        this.sendSocketNotification(this.name + 'VERSION_RESULTS', {
           local: version,
           remote,
         });
@@ -133,36 +145,63 @@ module.exports = NodeHelper.create({
     );
   },
 
-  fetchAll: function (config) {
-    config = {
-      ...this.defaults,
-      ...config,
-    };
-    clearInterval(this.interval);
-    Log.log(this.name, 'fetchAll', JSON.stringify(config));
-    this.interval = setInterval(() => {
-      config.live && this.fetchTotal(config.countryCodes);
-      config.world && this.fetchWorld();
-      this.fetchVersion();
-    }, config.updateInterval);
+  fetchAll: function(config = this.config) {
+    Log.info(this.name, 'fetchAll', JSON.stringify(config));
     config.live && this.fetchTotal(config.countryCodes);
     config.world && this.fetchWorld();
     this.fetchVersion();
   },
 
+  fetchAllWithInterval: function () {
+    try {
+      clearInterval(this.interval);
+      const fetch = () => {
+        this.fetchAll();
+        this.sendSocketNotification(this.name + 'NEXT_UPDATE', [new Date(), new Date(Date.now() + this.config.updateInterval)]);
+        Log.info(this.name, 'fetchAllWithInterval| next execution scheduled for', new Date(Date.now() + this.config.updateInterval));
+      }
+      this.interval = setInterval(fetch, this.config.updateInterval);
+      fetch();
+    }catch(e){
+      Log.error(this.name, 'fetchAllWithInterval', e)
+    }
+  },
+
+  fetchAllWithSchedule() {
+    try {
+      const schedule = require('node-schedule');
+      if (this.scheduler !== null) {
+        this.scheduler.cancel()
+      }
+
+      const fetch = () => {
+        this.fetchAll();
+        this.sendSocketNotification(this.name + 'NEXT_UPDATE', [new Date(), new Date(this.scheduler.nextInvocation())]);
+        Log.info(this.name, 'fetchAllWithSchedule | next execution scheduled for:', new Date(this.scheduler.nextInvocation()));
+      }
+
+      const scheduleConfig = this.config.schedulerConfig;
+      this.scheduler = schedule.scheduleJob(scheduleConfig, fetch)
+      fetch();
+    }catch(e) {
+      Log.error(this.name, 'fetchAllWithSchedule', e)
+    }
+  },
+
   socketNotificationReceived: function (notification, payload) {
     Log.debug(this.name, notification, JSON.stringify(payload));
-    if (notification === 'CONFIG') {
-      this.fetchAll(payload);
-    }
-    if (notification === 'GET_TOTAL') {
-      this.fetchTotal(payload || []);
-    }
-    if (notification === 'GET_WORLD') {
-      this.fetchWorld(payload || []);
-    }
-    if (notification === 'GET_VERSION') {
-      this.fetchVersion(payload || null);
+    if (notification === this.name + 'CONFIG') {
+      this.config = {
+        ...this.defaults,
+        ...payload,
+      };
+      Log.info(this.name, 'socketNotificationReceived', 'useScheduler:', payload.useScheduler);
+      if(payload.useScheduler) {
+        this.fetchAllWithSchedule(payload)
+      } else {
+        this.fetchAllWithInterval(payload);
+      }
+
     }
   },
 });
